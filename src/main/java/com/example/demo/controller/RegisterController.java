d63c1c73a11600dcbe82b3f730a71b79c0e3131d
endes <endes@192.168.1.11>
2020-06-27 22:39:41 +0900

    2020/6/27

--- src/main/java/com/example/demo/controller/RegisterController.java
+++ src/main/java/com/example/demo/controller/RegisterController.java
@@ -1,7 +1,11 @@
 package com.example.demo.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.dto.RegisterDto;
@@ -9,12 +13,28 @@ import com.example.demo.dto.RegisterDto;
 @Controller
 
 public class RegisterController {
+	
+	@PostMapping
+    public String create(@Valid @ModelAttribute RegisterDto registerDto, BindingResult bindingResult) { // ②
+        if(bindingResult.hasErrors()) return "register"; // ③
+        registerDtosave(registerDto);
+        return "redirect:/register";
+    }
 
+ 
+ 
+private void registerDtosave(@Valid RegisterDto registerDto) {
+	// TODO 自動生成されたメソッド・スタブ
+	
+}
+
+	
 	@ModelAttribute
     public RegisterDto setupForm() {
         return new RegisterDto();
     }
-
+	
+	
 	@RequestMapping(value="register")
 	public String registerDto() {
 		return "register";
--- src/main/java/com/example/demo/dto/RegisterDto.java
+++ src/main/java/com/example/demo/dto/RegisterDto.java
@@ -2,9 +2,6 @@ package com.example.demo.dto;
 
 import java.io.Serializable;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 import lombok.Data;
 
 @Data
@@ -14,25 +11,23 @@ public class RegisterDto implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 
-	 @NotNull // (1)対象のフィールドがnullでないことを示すjavax.validation.constraints.NotNullを付ける。
-	 @Size(min = 1, max = 20) // (2)対象のフィールドの文字列長が指定したサイズの範囲内にあることを示す
-	 private String name;
+	private String display_name;
 
-	 @NotNull
-	 @Size(min = 8)
-	 private String password;//	入力必須	8文字以上 confirmPasswordと同じ値であること
+	private String password;
+	private String confirmPassword;//確認用パスワード
 
-	 private String confirmPassword;//確認用パスワード
+	
+	 public Object getDisplay_name() {
+			// TODO 自動生成されたメソッド・スタブ
+			return null;
+		}
 
-	public Object getDisplay_name() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
-	}
+		public void setDisplay_name(Object display_name) {
+			// TODO 自動生成されたメソッド・スタブ
 
-	public void setDisplay_name(Object display_name) {
-		// TODO 自動生成されたメソッド・スタブ
+		}
 
-	}
+
 
 
 }
